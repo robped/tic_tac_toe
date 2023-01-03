ed55ac6e1d811043b12bb726d4a9a64f7f75c2b2
roberto <roberto.campedelli@gmail.com>
2023-01-03 22:06:26 +0100

    finishing setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square() {
+  const [value, setValue] = useState(null)
+  function handleClick() {
+    setValue('X')
+
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
 }
